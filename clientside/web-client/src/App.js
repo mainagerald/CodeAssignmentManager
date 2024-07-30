151d76cc863a26ad2e77efc9f0b5f7c3815df473
Flavian Maina <FLAVIAN.WAMBETI@equitybank.co.ke>
2024-07-30 10:46:38 +0300

    cors

--- clientside/web-client/src/App.js
+++ clientside/web-client/src/App.js
@@ -2,6 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  fetch("http://localhost:8888/api/auth/signin",{
+    headers:{
+      "Content-Type":"application/json"
+    },
+    method: "post"
+  });
+
   return (
     <div className="App">
       <p>We are coding!</p>
